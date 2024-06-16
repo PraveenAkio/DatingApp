944f710c89eb330f36ab49ff739eb9e6df2e89ac
Praveen Akio <praveenakio706@gmail.com>
2024-06-16 15:08:59 +0530

    Added Frontend Angular

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList(); // Async Not Working on This ToList().
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+    title = 'Client';
+    users: any;
+
+    constructor(private http: HttpClient) { }
+
+    ngOnInit(): void {
+        this.http.get("http://localhost:5077/api/users").subscribe({
+            next: response => this.users = response,
+            error: error => console.log(error),
+            complete: () => console.log("Request has Completed")
+        })
+    }
+}
