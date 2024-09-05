f0969d45c4718e4dd2ce2868943b65210ebf2b59
Praveen Akio <praveenakio706@gmail.com>
2024-09-05 16:14:42 +0530

    Added Like Feature

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from "@angular/forms";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { Pagination } from '../_models/pagination';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, MemberCardComponent, PaginationModule],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit{
+    members: Member[] | undefined;
+    predicate = 'liked';
+    pageNumber = 1;
+    pageSize = 5;
+    pagination: Pagination | undefined;
 
+    constructor(private memberService: MembersService) {}
+
+    ngOnInit(): void {
+        this.loadLikes();
+    }
+
+    loadLikes() {
+        this.memberService.getLikes(this.predicate, this.pageNumber, this.pageSize).subscribe({
+            next: response => {
+                this.members = response.result;
+                this.pagination = response.pagination;
+            }
+        })
+    }
+
+    pageChanged(event: any) {
+        if (this.pageNumber !== event.page) {
+            this.pageNumber = event.page;
+            this.loadLikes();
+        }
+    }
 }
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { RouterModule} from '@angular/router';
 import { Member } from '../../_models/member';
+import { MembersService } from '../../_services/members.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-card',
@@ -13,6 +15,14 @@ export class MemberCardComponent implements OnInit{
 
     @Input() member: Member | undefined;
 
+    constructor(private memberService: MembersService, private toastr: ToastrService) {}
+
     ngOnInit(): void {
+    }
+
+    addLike(member: Member) {
+        this.memberService.addLike(member.userName).subscribe({
+            next: () => this.toastr.success('You have liked ' + member.knownAs)
+        })
     }
 }
